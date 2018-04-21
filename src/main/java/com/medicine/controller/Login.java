0e2be467b308a7fdaef8a2eb1d700612179739e3
JalenLeeCN <34100298+JalenLeeCN@users.noreply.github.com>
2018-04-21 11:09:27 +0800

    20180421

--- src/main/java/com/medicine/controller/Login.java
+++
@@ -1,8 +0,0 @@
-package com.medicine.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller("/login")
-public class Login {
-	private 
-}
--- src/main/java/com/medicine/service/EnterService.java
+++
@@ -1,9 +0,0 @@
-package com.medicine.service;
-/**
- * 入口 : 登录 注册 重置密码
- * @author LZZ
- *
- */
-public interface EnterService {
-	public 
-}
