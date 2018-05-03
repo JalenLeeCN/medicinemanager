9897bfa99bf738d9f9db14bea8e0ffdf86ed6868
JalenLeeCN <34100298+JalenLeeCN@users.noreply.github.com>
2018-05-03 20:14:41 +0800

    20180503
    page

--- src/main/java/com/medicine/vo/Page.java
+++ src/main/java/com/medicine/vo/Page.java
@@ -4,7 +4,22 @@ public class Page {
 	private int currentPage;//当前页数
 	private int totalPage;//总页数
 	private int pageSize;//每页记录数
+	private int previousPage;//前一页
+	private int nextPage;//后一页
 	
+	
+	public int getPreviousPage() {
+		return previousPage;
+	}
+	public void setPreviousPage(int previousPage) {
+		this.previousPage = previousPage;
+	}
+	public int getNextPage() {
+		return nextPage;
+	}
+	public void setNextPage(int nextPage) {
+		this.nextPage = nextPage;
+	}
 	public int getCurrentPage() {
 		return currentPage;
 	}
